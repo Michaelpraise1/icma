60931f7ea93dd02c472bba5944969298d418c6f9
Michaelpraise1 <michaeld56567@gmail.com>
2025-07-30 10:07:39 -0700

    daraa

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,12 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/icma">
      <StrictMode>
       <App />
     </StrictMode>
   </BrowserRouter>
 
   
-   
-  
-  
+
 );
