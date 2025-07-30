09adc5577019b0b6f613349f3d9c4bf257f0e798
Michaelpraise1 <michaeld56567@gmail.com>
2025-07-30 10:13:15 -0700

    final

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,9 @@
-import About from "../component/About";
+import React from "react";
 import Footer from "../component/Footer";
 import Herosection from "../component/Herosection";
 import Services from "../component/Services";
 import Stat from "../component/Stat";
-import Testimonials from "../component/testimonials";
+
 import { FloatButton } from "antd";
 import Weem from "../component/Weem";
 import Hero from "../component/Hero";
@@ -13,11 +13,9 @@ const Homepage = () => {
     <div className=" bg-white">
       <Herosection />
       <Hero/>
-      {/* <About /> */}
       <Services />
       <Weem/>
       <Stat />
-      {/* <Testimonials /> */}
       <Footer />
       <FloatButton.BackTop />
     </div>
