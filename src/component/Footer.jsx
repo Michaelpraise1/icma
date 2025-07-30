5e25e5afc5893929565c1c8c6448693de137e68e
Michaelpraise1 <michaeld56567@gmail.com>
2025-07-30 14:11:38 -0700

    lastlast

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,4 @@
-import { FaTwitter } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa6";
-import { IoLogoDiscord } from "react-icons/io5";
-import { SiYoutube } from "react-icons/si";
+
 
 const Footer = () => {
   return (
