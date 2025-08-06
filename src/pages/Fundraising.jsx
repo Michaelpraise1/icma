316440bf627919f70e2c15d2a9033f4ca3f5e834
Michaelpraise1 <michaeld56567@gmail.com>
2025-08-06 14:22:16 -0700

    jdjdjrj

--- src/pages/Fundraising.jsx
+++ src/pages/Fundraising.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import Herosection from '../component/Herosection';
 import FundraisingSec from '../component/FundraisingSec';
-
 import { Link } from 'react-router-dom';
-import Bottom from '../component/bottom';
+
 
 const Fundraising = () => {
   return (
